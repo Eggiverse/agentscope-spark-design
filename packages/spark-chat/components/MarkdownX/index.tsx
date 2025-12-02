9cd37bfc2fb5ae59fdaa2f93bcc7652b96a8208e
lishengzxc <306009337@qq.com>
2025-12-02 22:51:02 +0800

    feat: enhance MarkdownX component with new media handling and code block rendering features

--- packages/spark-chat/components/MarkdownX/index.tsx
+++ packages/spark-chat/components/MarkdownX/index.tsx
@@ -1,10 +1,14 @@
-import { memo } from 'react';
+import { memo, useMemo } from 'react';
 import MarkdownX from './core';
 import type { MarkdownProps } from './types';
 import useTyping from './core/hooks/useTyping';
 import { useProviderContext } from '@agentscope-ai/chat';
 import classNames from 'classnames';
 import Styles from './styles';
+import Null from './core/components/Null';
+import CodeBlock from './core/components/CodeBlock';
+import DisabledImage from './core/components/DisableImage';
+import Media from './core/components/Media';
 
 
 export default memo(function (props: MarkdownProps) {
@@ -13,10 +17,17 @@ export default memo(function (props: MarkdownProps) {
   const content = useTyping({ content: props.content, typing: props.typing });
   const prefixCls = useProviderContext().getPrefixCls('markdown');
 
+  const components = useMemo(() => ({
+    pre: CodeBlock,
+    style: Null,
+    script: Null,
+    img: props.disableImage ? DisabledImage : Media,
+  }), [props.disableImage]);
 
   return <>
     <Styles />
     <MarkdownX
+      components={components}
       style={{ fontSize: baseFontSize, lineHeight: baseLineHeight }}
       openLinksInNewTab={true}
       className={classNames(prefixCls, props.className)}
